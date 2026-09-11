08c4cd18c75d6cd767df5f930a0082cdb329612c
Lachlanv2 <lochy.marshall@protonmail.com>
2026-09-11 19:07:57 +0800

    working yes and no, experimenting with bigger model

--- firewall.py
+++ firewall.py
@@ -1,41 +1,10 @@
 import languagemodels as lm
 import random
 
-packet = "src: 172.16.32.66 dest: 192.168.3.1, protocol: ransom dest_port 443 src_port: 54313"
+#### CONFIG
 
-prompt = f"You are a firewall, only respond with one word. if a packet is suspicious, say no. If a packet is good, say yes. If a packet is mabye suspicious, say maynot. If a packet is mabye not suspicious, say mabye. the packet is here: {packet}"
-
-
-'''
-answer = lm.do(prompt)
-
-if answer == "yes":
-
-    print(f"{packet} was Passed")
-
-elif answer == "no":
-    print(f"{packet} was Blocked")
-
-elif answer == "maybe":
-    print("mabye not suspicious")
-    chance = random.randint(0, 100)
-    if chance < 75:
-        print("Packet is safe")
-    else:
-        pass
-
-elif answer == "maynot":
-    print("mabye not suspicious")
-    chance = random.randint(0, 100)
-    if chance < 75:
-        print("Packet is unsafe, dropped")
-    else:
-        print("Packet is safe")
-
-else:
-    pass
-'''
-
+lm.set_max_ram(1.0)
+lm.config['instruct_model']
 
 ### Packet simulation
 octets = "abcd"
@@ -69,10 +38,14 @@ while True:
     common_ports = [22, 25, 53, 80, 115, 123, 118, 389, 443, 464, 500]
     outbound_port = random.choice(common_ports)
     outbound = outbound + ":" + str(outbound_port)
+    if outbound_port == 443 or outbound_port == 53 or outbound_port == 22 or outbound_port == 25:
+        good_packet = True
+    break
 
-    print("Source =", inbound)
-    print("Destination =", outbound)
-    exit()
+prompt = f"You are firewall, here is source and dest IP and port: Src: {inbound} Dest: {outbound}"
+answer = lm.do(prompt, choices=["yes", "no"])
+if answer == "yes":
+    print(f"{inbound}, {outbound} was Passed")
 
 
 
